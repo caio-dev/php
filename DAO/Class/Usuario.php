0f91a1ef6dc6df3d52c6e4fafe1fd45ccf5337b2
Caio Cesar <caio.cesar.dec15@gmail.com>
2020-05-08 23:06:49 -0300

    Atualização DAO

--- DAO/Class/Usuario.php
+++ DAO/Class/Usuario.php
@@ -48,27 +48,103 @@ class Usuario {
 		));
 
 		if (count($results) > 0){
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+	
+			$this->setData($results[0]);
 
 		}
 
 	}
 
+	public static function getList() {
 
-	public function  __toString(){
+		$sql = new Sql();
 
-		return json_encode(array(
-			"idusuario"=>$this->getIdusuario(),
-			"deslogin"=>$this->getDeslogin(),
-			"dessenha"=>$this->getDessenha(),
-			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+		return $sql->select("SELECT * FROM tb_usuarios");
 
+	}
+
+	public static function search($login) {
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH", array(
+			'SEARCH'=>"%".$login."%"
 		));
+
+	}
+
+	public function login($login, $password) {
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0){
+
+			$this->setData($results[0]);
+
+		} else {
+
+			throw new Exception("Login e/ ou senha incorretos");
+			
+		}
+
+	}
+
+	public function setData($data){
+
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+
+
+
+	public function insert(){
+
+		$sql = new Sql;
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if (count($results) > 0) {
+			$this->setData($results);
+		}
+	}
+
+	public function update($login, $password){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+		$sql = new Sql;
+ 
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'	=>$this->getDeslogin(),
+			':PASSWORD'	=>$this->getDessenha(),
+			':ID'		=>$this->getIdusuario()
+		));
+	}
+
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdusuario()
+		));
+
+		$this->setIdusuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
 
 	}
 
@@ -76,13 +152,29 @@ class Usuario {
 
 
 
+
+
+
+
+	public function __construct($login = "", $password = ""){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+	}
+
+	public function  __toString(){
+
+		return json_encode(array(
+			"idusuario"	=>$this->getIdusuario(),
+			"deslogin"	=>$this->getDeslogin(),
+			"dessenha"	=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+		));
+
+	}
+
 }
-
-
-
-
-
-
 
 
 ?>
--- DAO/index.php
+++ DAO/index.php
@@ -2,16 +2,56 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+/* Lista de usuarios por login
 
-$root->loadbyId(1);
+$search = Usuario::search("io");
+echo json_encode($search);
 
-echo($root);
+*/
 
+/* Lista completa de usuários
 
+ $lista = Usuario::getList();
+ echo json_encode($lista);
 
+*/
 
+/* A classe login esta dando erro
+$usuario = new Usuario();
+$usuario->login("Caio", "08041999");
 
+echo $usuario;
+*/
+
+/*
+echo "----------------------------<br>";
+
+$lista = new Usuario();
+$lista->loadbyId(1);
+echo $lista;
+*/
+
+/*
+// Cadastrar novo usuario
+$aluno = new Usuario("Maria", "abcde");
+$aluno->insert();
+echo $aluno;
+*/
+
+/*
+$atualizar = new Usuario();
+$atualizar->loadbyId(1);
+$atualizar->update("caio", "senhaaa");
+echo $atualizar;
+*/
+
+/*
+// Deletar usuário
+$delete = new Usuario();
+$delete->loadbyId(11);
+$delete->delete();
+echo $delete;
+*/
 
 
 
