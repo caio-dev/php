9618ec4b11c36f61799c9ccdc48fbe8af30f0abd
Caio Cesar <caio.cesar.dec15@gmail.com>
2020-05-13 16:56:40 -0300

    boleto e recaptcha

---
+++ captha/captcha.php
@@ -0,0 +1,11 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<form action="cadastrar.php" method="POST">
+
+
+	<input type="email" name="email">
+
+	<div class="g-recaptcha" data-sitekey="6Lee3vYUAAAAAD9uUJcDDsO_Xb8uiPWva8d3saCS"></div>
+
+	<button type="submit">Enviar</button>
+	
+</form>
